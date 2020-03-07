3184af4d40b5c44e04b834db2b42289798841d54
Milos Subotic <milos.subotic.sm@gmail.com>
2020-03-07 21:44:45 +0100

    Have working example.

--- FPGA/nios2_custom_pio/lprs2_pio.vhd
+++ FPGA/nios2_custom_pio/lprs2_pio.vhd
@@ -2,38 +2,126 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 
 library work;
 
 entity lprs2_pio is
 	port(
 		-- System.
-		clk                       :  in std_logic;
-		reset                     :  in std_logic;
+		clk              :  in std_logic;
+		reset            :  in std_logic;
 		
 		-- Avalon slave.
-		avs_chipselect                :  in std_logic;
+		avs_chipselect   :  in std_logic;
 		-- Word address.
-		avs_address                   :  in std_logic_vector(3 downto 0);
-		avs_write                     :  in std_logic;
-		avs_writedata                 :  in std_logic_vector(31 downto 0);
-		avs_read                      :  in std_logic;
-		avs_readdata                  : out std_logic_vector(31 downto 0);
+		avs_address      :  in std_logic_vector(3 downto 0);
+		avs_write        :  in std_logic;
+		avs_writedata    :  in std_logic_vector(31 downto 0);
+		avs_read         :  in std_logic;
+		avs_readdata     : out std_logic_vector(31 downto 0);
 		
 		-- Conduit.
-		coe_pi                        :  in std_logic_vector(7 downto 0);
-		coe_po                        : out std_logic_vector(7 downto 0)
+		coe_pi           :  in std_logic_vector(7 downto 0);
+		coe_po           : out std_logic_vector(7 downto 0)
 	);
 end entity;
 
 architecture lprs2_pio_arch of lprs2_pio is
 	
+	signal po                 : std_logic_vector(7 downto 0);
+	signal prev_pi            : std_logic_vector(7 downto 0);
+	signal pi                 : std_logic_vector(7 downto 0);
+	signal pi_change_occured  : std_logic_vector(7 downto 0);
+	signal pi_changed         : std_logic_vector(7 downto 0);
+	signal read_pi_changed    : std_logic;
+	signal read_pi_changed_d1 : std_logic;
 begin
 	
-	process(avs_address)
+	process(clk, reset)
 	begin
-		avs_readdata <= (others => '0');
-		avs_readdata(conv_integer(avs_address)) <= '1';
+		if reset = '1' then
+			po <= x"05";
+		elsif rising_edge(clk) then
+			if avs_chipselect = '1' and avs_write = '1' then
+				if avs_address < 8 then
+					po(conv_integer(avs_address)) <= avs_writedata(0);
+				elsif avs_address = 8 then
+					po <= avs_writedata(7 downto 0);
+				elsif avs_address = 10 then
+					if avs_writedata(0) = '0' then
+						po <= (others => '0');
+					else
+						po <= (others => '1');
+					end if;
+				elsif avs_address = 11 then
+					po <= not po;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	coe_po <= po;
+	
+	
+	process(clk, reset)
+	begin
+		if reset = '1' then
+			prev_pi <= x"00";
+			pi <= x"00";
+		elsif rising_edge(clk) then
+			prev_pi <= pi;
+			pi <= coe_pi;
+		end if;
+	end process;
+	
+	process(avs_address, pi, pi_changed)
+	begin
+		if avs_address < 8 then
+			avs_readdata <= conv_std_logic_vector(0, 31) &
+				pi(conv_integer(avs_address));
+		elsif avs_address = 8 then
+			avs_readdata <= x"000000" & pi;
+		elsif avs_address = 9 then
+			avs_readdata <= x"000000" & pi_changed;
+		elsif avs_address = 10 then
+			avs_readdata <= (others => '0');
+		elsif avs_address = 11 then
+			avs_readdata <= (others => '0');
+		else
+			avs_readdata <= x"babadeda";
+		end if;
+	end process;
+	
+	
+	read_pi_changed <= 
+		'1' when 
+			avs_chipselect = '1' and
+			avs_read = '1' and
+			avs_address = 9 else
+		'0';
+	process(clk, reset)
+	begin
+		if reset = '1' then
+			read_pi_changed_d1 <= '0';
+		elsif rising_edge(clk) then
+			read_pi_changed_d1 <= read_pi_changed;
+		end if;
+	end process;
+	
+	pi_change_occured <= prev_pi xor pi;
+	
+	process(clk, reset)
+	begin
+		if reset = '1' then
+			pi_changed <= x"00";
+		elsif rising_edge(clk) then
+			if read_pi_changed_d1 = '1' then
+				pi_changed <= x"00";
+			else
+				pi_changed <= pi_changed or pi_change_occured;
+			end if;
+		end if;
 	end process;
 
 end architecture;
--- FW/custom_pio_01_playground/custom_pio_01_playground.c
+++ FW/custom_pio_01_playground/custom_pio_01_playground.c
@@ -4,6 +4,8 @@
 #include "system.h"
 #include <stdio.h>
 
+#define WAIT_UNITL_0(x) while(x != 0){}
+#define WAIT_UNITL_1(x) while(x != 1){}
 
 int main() {
 	volatile uint32_t* pio = (volatile uint32_t*)SW_AND_LED_PIO_BASE;
@@ -14,18 +16,34 @@ int main() {
 	printf("\n");
 
 	pio[0] = 1;
-	printf("pio[0] = %u (0x%08x)\n", pio[0], pio[0]);
+	printf("pio[0] = 0x%08x\n", pio[0]);
 
 	pio[0] = 0;
-	printf("pio[0] = %u (0x%08x)\n", pio[0], pio[0]);
+	printf("pio[0] = 0x%08x\n", pio[0]);
+	printf("\n");
 
 
-	for(int i = 0; i < 16; i++){
+	for(int i = 0; i < 8; i++){
 		pio[i] = pio[i];
+	}
 
-		//TODO Print all regs.
+	printf("pio[9] = 0x%08x\n", pio[9]);
+	WAIT_UNITL_0(pio[0]);
+	WAIT_UNITL_1(pio[0]);
+	WAIT_UNITL_0(pio[0]);
+	printf("pio[9] = 0x%08x\n", pio[9]);
+	printf("pio[9] = 0x%08x\n", pio[9]);
+	printf("\n");
+
+	pio[11] = 0x10;
+
+/*
+	//TODO Print all regs.
+	for(int i = 0; i < 16; i++){
 		printf("pio[%d] = 0x%08x\n", i, pio[i]);
 	}
+	printf("\n");
+*/
 
 	return 0;
 }
